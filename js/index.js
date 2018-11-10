120dcb6d6787437022f68df73b858989496b9f36
Nick Smith <smthnck@gmail.com>
2018-11-10 18:00:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
+
