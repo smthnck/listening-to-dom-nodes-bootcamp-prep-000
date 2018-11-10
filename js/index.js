f4340eb7bcf2069fae452272b900839bce5e590e
Nick Smith <smthnck@gmail.com>
2018-11-10 18:01:16 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -12,3 +12,12 @@ input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
 
+const input = document.querySelector('input')
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault()
+  } else {
+    console.log(e.which)
+  }
+});
